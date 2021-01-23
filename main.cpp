9effd971799e7aceef797767ccbd89a1ce42fe95
emmanouilidisk <emmanouilidis.kons@gmail.com>
2021-01-24 00:11:46 +0200

    most recent code

--- main.cpp
+++ main.cpp
@@ -1,68 +1,124 @@
 #include <iostream>
+#include "ac_int.h"
+#include "ac_fixed.h"
 
-#define n 4 // number of users
-#define m 4 // number of movies
+const int n=4; // number of users
+const int m=4; // number of movies
+const int digits = 1; //number of digits of integer part
+const int total_num_digits = 15; //total number of digits
 
-// define arrays used in program
-float IR_old[m][n], IR_new[m][n];
-float movies_correlation[m][m]= { // denoted in the paper as C
-        {0.0,   1.0/6, 1.0/4, 1.0/5},
-        {1.0/3, 0,     2.0/4, 3.0/5},
-        {1.0/3, 2.0/6, 0,     1.0/5},
-        {1.0/3, 3.0/6, 1.0/4, 0}
-};
-//normalized movies ratings
-float initial_critics[m][n]= { //array with columns the vector d of each user
-        {1.0/6, 5.0/10, 0,      4.0/9},
-        {1.0/6, 0.0/10, 4.0/11, 1.0/9},
-        {4.0/6, 5.0/10, 3.0/11, 1.0/9},
-        {0.0,   0.0,   4.0/11, 3.0/9}
+
+template <typename T, int m>
+class vector{
+	private:
+		T *mem;
+		int pnt;
+	public:
+		vector(){
+			mem = NULL;
+			pnt = 0;
+		}
+		vector(T *pointer){
+			mem = pointer;
+			pnt = 0;
+		}
+		void write(ac_fixed<total_num_digits,digits,false> val) {  
+			pnt = (pnt + 1) % m;
+			mem[pnt] = val;
+		} 
+		T read() { 
+			pnt = (pnt - 1) % m;
+			return mem[pnt];
+		}		
 };
 
 
 class itemrank{
 private:
-    int max_iter = 20;
-    float a = 0.85;
-    float summation = 0;
+    int max_iter;
+    ac_fixed<total_num_digits,digits,false> a;
+    ac_fixed<total_num_digits,digits,false> summation, acc1, acc2;
+    ac_fixed<total_num_digits,digits,false> IR_col[m];
+    ac_fixed<total_num_digits,digits,false> IR_col_new[m];
+    ac_fixed<total_num_digits,digits,false> buffer_initial_critics[m];
 public:
     // constructor
     itemrank() {
+    	max_iter=20;
+    	a=0.85;
+    	summation=0;   	
         // initialize IR matrix
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                IR_old[j][i] = 1.0/m;
-            }
-        }
+       
     };
 
-    void run(){
 
-        for(int user = 0; user < n; user++) { //for each user
-            for (int t = 0; t < max_iter; t++) { //for each time
-                for(int i = 0; i < m; i++) { //for each movie
-                    summation = 0;
-                    for(int j = 0; j < m; j++) {
-                        summation = summation + movies_correlation[i][j] * IR_old[j][user];
+    void run (ac_fixed<total_num_digits,digits,false> movies_correlation[m][n],ac_fixed<total_num_digits,digits,false> initial_critics[m][n],ac_fixed<total_num_digits,digits,false> IR_new[m][n], ac_fixed<total_num_digits,digits,false> IR_col_pointer[m], ac_fixed<total_num_digits,digits,false> IR_col_new_pointer[m]){
+		
+		// IR(0) = 1.0/m
+		// IR(t+1) = a * C * IR(t) + (1-a) * d
+		
+        USER:for(int user = 0; user < n; user++) { //for each user
+        
+        	// declare vector
+			vector <ac_fixed<total_num_digits,digits,false>, m> IR_col(IR_col_pointer);
+			vector <ac_fixed<total_num_digits,digits,false>, m> IR_col_new(IR_col_new_pointer);
+			// initialize buffer IR
+            IR_COL_INIT:for (int j = 0; j < n; j++) {
+                IR_col.write(1.0/m);
+            }
+            TIME:for(int t = 0; t < max_iter; t++) { //for each time
+                MOVIE:for(int i = 0; i < m; i++) { //for each movie
+                	summation = 0;
+                    //acc1 = 0;
+                    //acc2 = 0;
+                    INNER_PRODUCT: for(int j = 0; j < m; j++) {
+                        //acc1 = acc2 + movies_correlation[i][j] * IR_col.read();
+                        //acc2 = acc1 + movies_correlation[i][j+1] * IR_col.read();
+                        summation = summation + movies_correlation[i][j] * IR_col.read();
                     }
-                    IR_new[i][user] = a * summation + (1-a)*initial_critics[i][user]; //rating of i-th movie of user
+                    //summation = acc1 + acc2;
+                    IR_col_new.write(a * summation + (1-a)*initial_critics[i][user]); //rating of i-th movie of user
                 }
-                for(int i = 0; i < m; i++){
-                    for(int j = 0; j < n; j++){
-                        IR_old[i][j] = IR_new[i][j];
-                    }
+                UPDATE_IR:for(int j = 0; j < m; j++) {
+                	IR_col.write(IR_col_new.read());	
                 }
+            }
+            // write to output
+            WRITE_OUTPUT:for(int i = 0; i < m; i++){
+                IR_new[i][user] = IR_col.read();
             }
         }
     }
 };
 
 
-int main() {
+int main(){
 
+	// define arrays used in program
+    ac_fixed<total_num_digits,digits,false> IR_new[m][n];
+    ac_fixed<total_num_digits,digits,false> IR_col_pointer[m],IR_col_new_pointer[m];
+    ac_fixed<total_num_digits,digits,false> movies_correlation[m][m]= { // denoted in the paper as C
+	        {0.0,   1.0/6, 1.0/4, 1.0/5},
+	        {1.0/3, 0,     2.0/4, 3.0/5},
+	        {1.0/3, 2.0/6, 0,     1.0/5},
+	        {1.0/3, 3.0/6, 1.0/4, 0}
+	};
+	//normalized movies ratings
+    ac_fixed<total_num_digits,digits,false> initial_critics[m][n]= { //array with columns the vector d of each user
+	        {1.0/6, 5.0/10, 0,      4.0/9},
+	        {1.0/6, 0.0/10, 4.0/11, 1.0/9},
+	        {4.0/6, 5.0/10, 3.0/11, 1.0/9},
+	        {0.0,   0.0,   4.0/11, 3.0/9}
+	};
+	// initialize 
+        for (int j = 0; j < m; j++) {
+            IR_col_pointer[j] = 1.0/m;
+            IR_col_new_pointer[j] = 1.0/m;
+        }
+	
     // create object and run Itemrank algorithm
     itemrank obj;
-    obj.run();
+    obj.run(movies_correlation,initial_critics,IR_new,IR_col_pointer,IR_col_new_pointer);
 
     // print results
     for(int i = 0; i < m; i++){
@@ -73,5 +129,6 @@ int main() {
     }
 
 
-    return 0;
+   return(0);
 }
+
